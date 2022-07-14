c9621327b4418527be1db063fb9b1814067046b4
HandelFan <jegan.sivakasiulaganathan@gmail.com>
2022-07-14 01:49:08 -0400

    Near final changes

--- Program.cs
+++ Program.cs
@@ -1,40 +1,65 @@
 ﻿using HtmlAgilityPack;
 using System.Web;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace WebCrawler
 {
     internal class Program
     {
-        
-        internal Dictionary<string, int> word_dict  = new Dictionary<string, int>();
-        internal static string[] skippable_types = {"style", "link", "sup"};
-        static void Main(string[] args)
-        {
-            var start_xpath = @"//*[@id='History']/parent::h2";
-            var end_xpath = @"//*[@id='cite_ref-:0_155-1']";
+        private const string start_xpath = @"//span[@id='History']/parent::h2";
+        private const string end_xpath = @"//span[@id='Corporate_affairs']/parent::h2/preceding::p[1]";
+        private static string[] skippable_types = new string[]{"style", "link", "sup"};
+        private static Dictionary<string, int> word_dict  = new Dictionary<string, int>();
+        internal static Dictionary<string, int> Word_Dict {
+            get{ return word_dict; }
+            set{ word_dict = value; }
+        }
+        internal static string append_path = "output/log.txt";
 
-            //consider making below global
-            var html_doc = new HtmlDocument();
-            var wikipedia_html = get_wikipedia_html();
-            if (wikipedia_html == null ) {
+        //Use the field below to specify how many words you would like to return!
+        internal static int num_words = 10;
+
+        //Use the field below to specify any words you'd like to exclude from the search!
+        internal static string[] excluded_words = {};
+
+        static void Main(string[] args)
+        {            
+            HtmlDocument? wiki_html_doc = get_load_html_doc();
+            if(wiki_html_doc == null){
+                Console.WriteLine("Unable to access Wikipedia. Exiting.");
                 return;
             }
-            html_doc.LoadHtml(wikipedia_html);
-            var crawler = html_doc.DocumentNode.SelectSingleNode(start_xpath);
-            var end_node = html_doc.DocumentNode.SelectSingleNode(end_xpath);
-            scrape_runner(crawler, end_node);
+            scrape_runner(wiki_html_doc);
+
+            using (StreamWriter writer = new StreamWriter(@"output/out.txt"))
+            {
+                var sorted_descending_enumerable = (from kvp in Word_Dict orderby kvp.Value descending select kvp).Take(Int32.Parse(num_words);
+                foreach (KeyValuePair<string, int> kvp in sorted_descending_enumerable)
+                {
+                    writer.WriteLine(kvp.Key + ": " + kvp.Value);
+                }
+            }
         }
 
-        static void debug_test() {
+        static HtmlDocument? get_load_html_doc()
+        {
             var html_doc = new HtmlDocument();
-            var wikipedia_html = get_wikipedia_html();
-            html_doc.LoadHtml(wikipedia_html);
-            var body_xpath = @"//body";
-            var body= html_doc.DocumentNode.SelectSingleNode(body_xpath);
-            var body_tags = body.Descendants("*");
-            //var count = body_tags.
-            //for (var i = 0; i < html_doc.Count<body_tags>; )
+            try
+            {
+                var wikipedia_html = get_wikipedia_html();
+                if (wikipedia_html == null)
+                {
+                    throw new Exception("Unknown error accessing wikipedia!");
+                }
+                html_doc.LoadHtml(wikipedia_html);
+            }
+            catch(Exception e) {
+                Console.WriteLine("Unable to load HTML. Encountered the following exception:");
+                Console.WriteLine(e.Message.ToString());
+                return null;
+            }
+            return html_doc;
         }
 
         static string? get_wikipedia_html() {
@@ -51,63 +76,51 @@ namespace WebCrawler
             }
         }
 
-        static string scrape_runner(HtmlNode start_node, HtmlNode end_node)
+        static string scrape_runner(HtmlDocument wiki_html_doc)
         {
-            string path = @"web-crawler-log/log.txt";
-            using (StreamWriter writer = new StreamWriter(path))
+            var start_node = wiki_html_doc.DocumentNode.SelectSingleNode(start_xpath);
+            var end_node = wiki_html_doc.DocumentNode.SelectSingleNode(end_xpath);
+            while (start_node != end_node)
             {
-                while (start_node != end_node)
+                while (skippable_types.Contains(start_node.Name) && start_node.NextSibling != null)
                 {
-                    while (skippable_types.Contains(start_node.Name))
-                    {
-                        start_node = start_node.NextSibling;
-                    }
-
-                    Console.WriteLine("Element name: " + start_node.Name);
-                    writer.WriteLine("Element name: " + start_node.Name);
-
-                    scrape_nodes(start_node.DescendantsAndSelf().ToArray());
-
-                    if (start_node.NextSibling == null)
-                    {
-                        break;
-                    }
                     start_node = start_node.NextSibling;
-
-                    writer.WriteLine("");
-                    Console.WriteLine("");
                 }
+
+                Console.WriteLine("Element name: " + start_node.Name);
+
+                scrape_nodes(start_node.DescendantsAndSelf().ToArray());
+
+                if (start_node.NextSibling == null)
+                {
+                    break;
+                }
+                start_node = start_node.NextSibling;
+                Console.WriteLine("");
             }
+            scrape_nodes(start_node.DescendantsAndSelf().ToArray());
             return "";
         }
 
         static void scrape_nodes(HtmlNode[] descendants)
         {
-            string path = @"web-crawler-log/log.txt";
-            using (StreamWriter writer = new StreamWriter(path))
+            for (int i = 0; i < descendants.Length; i++)
             {
-                for (int i = 0; i < descendants.Length; i++)
+                if (skippable_types.Contains(descendants[i].Name))
                 {
-                    if (skippable_types.Contains(descendants[i].Name))
-                    {
-                        if (descendants[i].HasChildNodes)
-                        {
-                            foreach (HtmlNode skippable_type_descendant in descendants[i].Descendants())
-                            {
-                                i++;
-                            }
-                            break;
-                        }
-                        continue;
-                    }
-                    if (skip_useless_valid_descendant(descendants[i]))
+                    if (descendants[i].HasChildNodes)
                     {
                         continue;
                     }
-                    string[] arr = format_scraped_string(descendants[i].InnerText);
-                    Console.WriteLine("      InnerHtml: " + HttpUtility.HtmlDecode(descendants[i].InnerText));
-                    writer.WriteLine("      InnerHtml: " + HttpUtility.HtmlDecode(descendants[i].InnerText));
+                    continue;
                 }
+                if (skip_useless_valid_descendant(descendants[i]))
+                {
+                    continue;
+                }
+                add_to_dictionary(match_scraped_string(descendants[i].InnerText));
+                Console.WriteLine("      InnerHtml: " + HttpUtility.HtmlDecode(descendants[i].InnerText));
+                File.AppendAllText(append_path, HttpUtility.HtmlDecode(descendants[i].InnerText));
             }
         }
 
@@ -115,34 +128,23 @@ namespace WebCrawler
             return node.NodeType != HtmlNodeType.Text || node.InnerHtml == null || node.InnerHtml == "";
         }
 
-        static string[] format_scraped_string(string s) {
-            return Regex.Matches(s, @"\b[\w']+\b")
+        static string[] match_scraped_string(string s) {
+            return Regex.Matches(s, @"\b[A-Za-z]+\b")
                 .Cast<Match>()
                 .Select(m => m.Value)
                 .ToArray();
         }
 
-        static void add_to_dictionary()
+        static void add_to_dictionary(string[] arr)
         {
-
-        }
-
-        static string StripHTML(string htmlStr)
-        {
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(htmlStr);
-            var root = doc.DocumentNode;
-            string s = "";
-            foreach (var node in root.DescendantsAndSelf())
+            foreach (string s in arr)
             {
-                if (!node.HasChildNodes)
+                if (!Word_Dict.ContainsKey(s))
                 {
-                    string text = node.InnerText;
-                    if (!string.IsNullOrEmpty(text))
-                        s += text.Trim() + " ";
+                    Word_Dict.Add(s, 1);
                 }
+                Word_Dict[s]++;
             }
-            return s.Trim();
         }
     }
 }
